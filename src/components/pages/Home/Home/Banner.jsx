e586b163b9971f5528e244fb3034c73469cc1536
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-12-22 13:20:07 +0600

    add the explore button

--- src/components/pages/Home/Home/Banner.jsx
+++ src/components/pages/Home/Home/Banner.jsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import banner from "../../../../assets/images/banner.jpg";
+import { AuthContext } from "../../../../provider/AuthProvider";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
+  const { user } = useContext(AuthContext);
   return (
     <section className="max-h-screen mb-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2 items-center ">
-        <div className="text-left px-10">
+        <div className="text-left px-10 space-y-3">
           <h1 className="text-2xl font-bold">Task Management</h1>
           <p className="text-lg font-medium">
             Task management involves organizing and prioritizing activities to
@@ -14,6 +18,19 @@ const Banner = () => {
             ensures that resources are utilized optimally and helps individuals
             or teams stay focused on their objectives.{" "}
           </p>
+          {user ? (
+            <>
+              <Link to="/dashboard">
+                <button className="btn btn-primary">Let &apos;s Explore</button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/login">
+                <button className="btn btn-primary">Let &apos;s Explore</button>
+              </Link>
+            </>
+          )}
         </div>
         <div>
           <img src={banner} alt="" className="mb-10" />
